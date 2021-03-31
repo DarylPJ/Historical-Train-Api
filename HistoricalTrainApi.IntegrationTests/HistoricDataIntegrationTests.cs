338de9e219c61dfd8ae23ffeaf5342b0466d9415
DarylPJ <34217093+DarylPJ@users.noreply.github.com>
2021-03-31 16:07:47 +0100

    Add integration tests

---
+++ HistoricalTrainApi.IntegrationTests/HistoricDataIntegrationTests.cs
@@ -0,0 +1,98 @@
+﻿using DeepEqual.Syntax;
+using HistoricalTrainApi.Models;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Newtonsoft.Json;
+using RichardSzalay.MockHttp;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HistoricalTrainApi.IntegrationTests
+{
+    public class HistoricDataIntegrationTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        const string serviceMetricsUri = "https://hsp-prod.rockshore.net/api/v1/serviceMetrics";
+        const string serviceDetailsUri = "https://hsp-prod.rockshore.net/api/v1/serviceDetails";
+
+        private readonly WebApplicationFactory<Startup> factory;
+
+        public HistoricDataIntegrationTests(WebApplicationFactory<Startup> factory)
+        {
+            this.factory = factory;
+        }
+
+        private HttpClient GetTestClientwithExternalclientMocked(HttpClient externalHttpClient)
+        {
+            var httpClientFactory = Mock.Of<IHttpClientFactory>(
+                i => i.CreateClient(It.IsAny<string>()) == externalHttpClient);
+
+            return factory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureTestServices(services =>
+                {
+                    services.AddSingleton<IHttpClientFactory>(httpClientFactory);
+                });
+            }).CreateClient();
+        }
+
+        [Fact]
+        public async Task ExternalHistoricServiceMetricsDown_ReturnsServiceUnavailable()
+        {
+            var mockHttp = new MockHttpMessageHandler();
+            mockHttp.When(serviceMetricsUri)
+                .Respond(HttpStatusCode.InternalServerError);
+
+            var client = GetTestClientwithExternalclientMocked(mockHttp.ToHttpClient());
+
+            var response = await client.GetAsync("/HistoricalData?startLocation=CFL&EndLocation=LDS&startDate=2021/03/25 07:00&endDate=2021/03/25 07:30");
+
+            Assert.Equal(HttpStatusCode.ServiceUnavailable, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task ExternalHistoricServiceDetailsDown_ReturnsServiceUnavailable()
+        {
+            var mockHttp = new MockHttpMessageHandler();
+            mockHttp.When(serviceMetricsUri)
+                .Respond("application/json", JsonConvert.SerializeObject(HistoricalServiceMockData.GetServiceMetricsResponse()));
+
+            var serviceDetailsRequest = mockHttp.When(serviceDetailsUri)
+                .Respond(HttpStatusCode.InternalServerError);
+
+            var client = GetTestClientwithExternalclientMocked(mockHttp.ToHttpClient());
+
+            var response = await client.GetAsync("/HistoricalData?startLocation=CFL&EndLocation=LDS&startDate=2021/03/25 07:00&endDate=2021/03/25 07:30");
+
+            Assert.Equal(503, (int)response.StatusCode);
+            Assert.Equal(1, mockHttp.GetMatchCount(serviceDetailsRequest));
+        }
+
+        [Fact]
+        public async Task ExternalHistoricServiceWorking_ExpectedDataReturned()
+        {
+            var mockHttp = new MockHttpMessageHandler();
+            mockHttp.When(serviceMetricsUri)
+                .Respond("application/json",
+                JsonConvert.SerializeObject(HistoricalServiceMockData.GetServiceMetricsResponse()));
+
+            mockHttp.When(serviceDetailsUri)
+                .Respond("application/json",
+                JsonConvert.SerializeObject(HistoricalServiceMockData.GetServiceDetailsResponse()));
+
+            var client = GetTestClientwithExternalclientMocked(mockHttp.ToHttpClient());
+
+            var response = await client.GetAsync("/HistoricalData?startLocation=CFL&EndLocation=LDS&startDate=2021/03/25 07:00&endDate=2021/03/25 07:30");
+
+            response.EnsureSuccessStatusCode();
+            var content = await response.Content.ReadAsStringAsync();
+            var actualResponse = JsonConvert.DeserializeObject<IList<HistoricalRecord>>(content);
+
+            actualResponse.ShouldDeepEqual(HistoricalServiceMockData.GetExpectedApiResponse());
+        }
+    }
+}
---
+++ HistoricalTrainApi.IntegrationTests/HistoricalServiceMockData.cs
@@ -0,0 +1,182 @@
+﻿using HistoricalTrainApi.Models;
+using HistoricalTrainApi.Models.HistoricService.ServiceDetails;
+using HistoricalTrainApi.Models.HistoricService.ServiceMetrics;
+using System.Collections.Generic;
+
+namespace HistoricalTrainApi.IntegrationTests
+{
+    public static class HistoricalServiceMockData
+    {
+        public static ServiceMetricsResponse GetServiceMetricsResponse() =>
+            new ServiceMetricsResponse
+            {
+                Header = new Header
+                {
+                    FromLocation = "CFL",
+                    ToLocation = "LDS"
+                },
+                Services =
+                {
+                    new Service
+                    {
+                        ServiceAttributesMetrics = new ServiceAttributesMetrics
+                        {
+                            OriginLocation = "SKI",
+                            DestinationLocation = "LDS",
+                            TimetabledDeparture = "0719",
+                            TimetabledArrival = "0744",
+                            TrainOperator = "NT",
+                            MatchedServices = 1,
+                            Rids =
+                            {
+                                "202103257842095"
+                            }
+                        },
+                        Metrics =
+                        {
+                            new Metric
+                            {
+                                ToleranceValue = 0,
+                                NumberOutsideTolerance = 0,
+                                NumberInsideTolerance = 1,
+                                PercentageInsideTolerance = 100,
+                                GlobalTolerance = true
+                            }
+                        }
+                    }
+                }
+            };
+
+        public static ServiceDetailsResponse GetServiceDetailsResponse() =>
+            new ServiceDetailsResponse
+            {
+                ServiceAttributesDetails = new ServiceAttributesDetails
+                {
+                    DateOfService = "2021-03-25",
+                    TrainOperator = "NT",
+                    Rid = "202103257842095",
+                    Locations =
+                    {
+                        new LocationDetail
+                        {
+                            Location = "SKI",
+                            TimetabledDeparture = "0701",
+                            TimetabledArrival = "",
+                            ActualDeparture = "0701",
+                            ActualArrival = "",
+                            DelayReason = ""
+                        },
+                        new LocationDetail
+                        {
+                            Location = "CEY",
+                            TimetabledDeparture = "0706",
+                            TimetabledArrival = "0706",
+                            ActualDeparture = "0705",
+                            ActualArrival = "0704",
+                            DelayReason = ""
+                        },
+                        new LocationDetail
+                        {
+                            Location = "SON",
+                            TimetabledDeparture = "0711",
+                            TimetabledArrival = "0711",
+                            ActualDeparture = "0710",
+                            ActualArrival = "0709",
+                            DelayReason = ""
+                        },
+                        new LocationDetail
+                        {
+                            Location = "KEI",
+                            TimetabledDeparture = "0715",
+                            TimetabledArrival = "0715",
+                            ActualDeparture = "0715",
+                            ActualArrival = "0714",
+                            DelayReason = ""
+                        },
+                        new LocationDetail
+                        {
+                            Location = "CFL",
+                            TimetabledDeparture = "0719",
+                            TimetabledArrival = "0719",
+                            ActualDeparture = "0718",
+                            ActualArrival = "0717",
+                            DelayReason = ""
+                        },
+                        new LocationDetail
+                        {
+                            Location = "BIY",
+                            TimetabledDeparture = "0722",
+                            TimetabledArrival = "0721",
+                            ActualDeparture = "0721",
+                            ActualArrival = "0720",
+                            DelayReason = ""
+                        },
+                        new LocationDetail
+                        {
+                            Location = "SAE",
+                            TimetabledDeparture = "0726",
+                            TimetabledArrival = "0726",
+                            ActualDeparture = "0725",
+                            ActualArrival = "0725",
+                            DelayReason = ""
+                        },
+                        new LocationDetail
+                        {
+                            Location = "SHY",
+                            TimetabledDeparture = "0729",
+                            TimetabledArrival = "0728",
+                            ActualDeparture = "0729",
+                            ActualArrival = "0728",
+                            DelayReason = ""
+                        },
+                        new LocationDetail
+                        {
+                            Location = "APY",
+                            TimetabledDeparture = "0734",
+                            TimetabledArrival = "0733",
+                            ActualDeparture = "0733",
+                            ActualArrival = "0733",
+                            DelayReason = ""
+                        },
+                        new LocationDetail
+                        {
+                            Location = "LDS",
+                            TimetabledDeparture = "",
+                            TimetabledArrival = "0744",
+                            ActualDeparture = "",
+                            ActualArrival = "0743",
+                            DelayReason = ""
+                        }
+                    }
+                }
+            };
+
+        public static IList<HistoricalRecord> GetExpectedApiResponse() => new List<HistoricalRecord>
+        {
+            new HistoricalRecord
+            {
+                OriginLocation = "SKI",
+                DestinationLocation = "LDS",
+                LocationData =
+                {
+                    ["CFL"] = new HistoricalData
+                    {
+                        TimetabledDeparture = "0719",
+                        TimetabledArrival = "0719",
+                        ActualDeparture = "0718",
+                        ActualArrival = "0717",
+                        DelayReason = ""
+                    },
+                    ["LDS"] = new HistoricalData
+                    {
+                        TimetabledDeparture = "",
+                        TimetabledArrival = "0744",
+                        ActualDeparture = "",
+                        ActualArrival = "0743",
+                        DelayReason = ""
+                    }
+                }
+            }
+        };
+    }
+}
